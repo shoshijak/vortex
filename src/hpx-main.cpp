a8d98df983f77bd1b3f83662b448fbcf48002c28
shoshijak <shoshana.jakobovits@protonmail.ch>
2017-04-04 11:42:20 +0200

    vortex is running in HPX!

--- src/hpx-main.cpp
+++ src/hpx-main.cpp
@@ -10,19 +10,33 @@
 #include <cassert>
 #include <cstring>
 
-#include <algorithm>                    // STL
-#include <limits>
-#include <vector>
+#include "simulation.h"
 
-#include "timer.h"                      // vrtx
-#include "tree.h"
-#include "kernels.h"
 
 int hpx_main()
 {
     hpx::cout << "Starting HPX application - VORTEX with HPX\n";
 
-    //!
+    double theta = 0.5;
+    bool verify = true;
+    double tol_ = 1e-8; // is supposed to be the same as tol in run_tests.cpp, but I was having linker errors
+
+    char filename[256];
+  	strcpy(filename, "test-data/dN400");
+
+  	if (access(filename, R_OK) == -1)
+  	{
+  		printf("WARNING: reference file <%s> not found.\n", filename);
+  		return 1;
+  	}
+  	else
+  		printf("reading from <%s> ...\n", filename);
+
+  	FILE * fin = fopen(filename, "r");
+
+  	assert(fin && sizeof(double) == sizeof(double));
+
+  	test(theta, tol_, fin, verify);
 
     return hpx::finalize();
 }
